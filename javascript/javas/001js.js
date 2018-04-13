ebcf224632bc7c321b6a4dd32eb51fff74017c70
zhu <1404619665@qq.com>
2018-04-13 21:08:51 +0800

    js file

---
+++ javascript/javas/001js.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+			function toblue(){
+				document.getElementById('box').style.backgroundColor = 'blue';
+			}
+			document.getElementById('box').onclick = toblue;
+		}
+		window.onload=function(){
+			function tored(){
+				document.getElementById('box2').style.backgroundColor = 'red';
+			}
+			document.getElementById('box2').onmouseover = tored;
+		}
